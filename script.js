911321949e97dbf702f3cd04110b6f2bfe40b3ab
Paul Garrod <paulrgarrod@gmail.com>
2021-07-23 16:45:40 +1200

    update commenting
    Update code comments throughout and final check.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// tiger checkbox
 const tigerSelector = document.querySelector("#tiger");
-const tigerTextbox = document.querySelector("#tiger-textbox");
+
+// text box toggle for tiger selector
 const toggleTextbox = () => {
+    // hidden text box for tiger checkbox
+    const tigerTextbox = document.querySelector("#tiger-textbox");
+    // conditional to add or remove the required attribute to the hidden tiger text box
     if (tigerSelector.checked){
         tigerTextbox.setAttribute("required", "required");
     } else {
         tigerTextbox.removeAttribute("required");
     }
+    // ternery operator to toggle between displays
     tigerTextbox.style.display = tigerSelector.checked === true ? "block" : "none";
 }
+// run toggle function when tiger checkbox is checked
 tigerSelector.addEventListener("click", toggleTextbox);
